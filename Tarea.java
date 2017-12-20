62c56086052f6846f270b0a777afe8b28f658882
davidrodriguez94 <rodriguezdiezdavid94@gmail.com>
2017-12-20 13:32:23 +0100

    Clase Tarea añadida y Todoist refactorizada

---
+++ Tarea.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Tarea here.
+ *
+ * @author (David Rodriguez)
+ * @version (a version number or a date)
+ */
+public class Tarea
+{
+    private String descripcionTarea;
+    private boolean marcarComoCompletada;
+
+    /**
+     * Constructor for objects of class Tarea
+     */
+    public Tarea()
+    {
+        
+    }
+}
